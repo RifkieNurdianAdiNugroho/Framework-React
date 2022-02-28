b7b4378416b9b1df7cf8b0b6f5f3d726f8127a5f
= <=>
2022-02-28 17:05:12 +0700

    commit baru

--- Week3/web-reactku/src/LOGIN/Login.js
+++ Week3/web-reactku/src/LOGIN/Login.js
@@ -8,11 +8,15 @@ const Login = () =>{
             <center> <h1 class="tulisan_login">Tugas Pertemuan Ketiga</h1></center>
 
             <form>
-                <label>Username</label>
-                <center><input type="text" name="username" class="form_login" placeholder="Username atau email .."/></center>
+                <div class="form-inline">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form_login" placeholder="masukkan username"/>
+                </div>
 
-                <label>Password</label>
-                <input type="text" name="password" class="form_login" placeholder="Password .."/>
+                <div class="form-inline">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form_login" placeholder="masukkan password"/>
+                </div>
 
                 <input type="submit" class="tombol_login" value="LOGIN"/>
 

3f683a41ad48b6702114a9bd3b7c10f63e0107cd
= <=>
2022-05-16 22:21:20 +0700

    add newest code and some new folder

--- Week11/firebase2/src/called/configureStore.js
+++ Week11/firebase2/src/called/configureStore.js
@@ -2,12 +2,13 @@ import { applyMiddleware, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { verifyAuth } from "./actions/";
 import rootReducer from "./reducers";
+
 export default function configureStore(persistedState) {
- const store = createStore(
- rootReducer,
- persistedState,
- applyMiddleware(thunkMiddleware)
- );
- store.dispatch(verifyAuth());
- return store;
+    const store = createStore(
+        rootReducer,
+        persistedState,
+        applyMiddleware(thunkMiddleware)
+    );
+    store.dispatch(verifyAuth());
+    return store;
 }

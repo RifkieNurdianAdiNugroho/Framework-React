a3bec9872f6dc48df953450a31a13ab13fb502a8
= <=>
2022-05-16 22:02:17 +0700

    add some new code this week

---
+++ Week11/firebase2/src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3v5SUjJwhHaCG-UzFRPjnoRbClBknMdI",
+    authDomain: "fir-rifkie.firebaseapp.com",
+    projectId: "fir-rifkie",
+    storageBucket: "fir-rifkie.appspot.com",
+    messagingSenderId: "48225544177",
+    appId: "1:48225544177:web:1114bd61313d6e20a58650"
+  };
